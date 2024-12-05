c385396a51c250652a430fbe57f43be183021fcb
OnofreTZK <taraujonofre@gmail.com>
2024-12-04 21:02:51 -0300

    nix setup

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Lambda Calculus Flake";
+
+  inputs = {
+    nixpkgs.url = "github:anmonteiro/nix-overlays";
+    nix-filter.url = "github:numtide/nix-filter";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nix-filter, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = (nixpkgs.makePkgs {
+        inherit system;
+      }).extend (self: super: {
+        ocamlPackages = super.ocaml-ng.ocamlPackages_5_2;
+      }); in
+      let grainhack = pkgs.callPackage ./nix {
+        inherit nix-filter;
+        doCheck = true;
+      }; in
+      rec {
+        packages = { inherit grainhack; };
+        devShell = import ./nix/shell.nix { inherit pkgs grainhack; };
+      });
+}
