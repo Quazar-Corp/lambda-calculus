0b357fe6237b52fbe47f8debe039b50ff125b971
OnofreTZK <taraujonofre@gmail.com>
2024-12-14 20:10:50 -0300

    Clean main

--- src/main.ml
+++ src/main.ml
@@ -1,20 +1,3 @@
-open Formal.Description
+open Lexical
 
-let () = print_endline "Hello, Lambda Calculus"
-let identity = Function ("x", Name "x") |> to_string
 
-let identity_applied =
-  Application (Function ("x", Name "x"), Name "y") |> to_string
-
-let () = Printf.printf "Identity function: %s\n" identity
-let () = Printf.printf "Identity applied to y: %s\n" identity_applied
-
-let () =
-  Printf.printf "Tokenized Identity:\n";
-  "λx.x" |> Lexer.tokenize
-  |> List.iter (fun l -> Printf.printf "LEXEME: %s\n" (Lexer.show_token l))
-
-let () =
-  Printf.printf "Tokenized Identity Applied:\n";
-  "(λx.x)y" |> Lexer.tokenize
-  |> List.iter (fun l -> Printf.printf "LEXEME: %s\n" (Lexer.show_token l))
