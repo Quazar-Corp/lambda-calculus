e943f6896c1d0ebac36b65af2a4209544f15afb1
OnofreTZK <taraujonofre@gmail.com>
2024-12-21 16:52:17 -0300

    Parser working

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,16 @@
-open Lexical
+open Frontend.Lexer
+open Core
 
+let extension = ".lambda"
+let check_extension filename = Filename.check_suffix filename extension
 
+let main args =
+  let filename = args.(1) in
+  if filename |> check_extension then
+    let input = In_channel.read_all filename in
+    parse input
+  else
+    Printf.eprintf
+      "Invalid file! Please provide a valid lambda calculus file (.lambda)\n"
+
+let () = Sys.get_argv () |> main
